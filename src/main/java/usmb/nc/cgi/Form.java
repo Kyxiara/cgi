b2e5ddaf09fbf7a2b909c1129e60623adc683bca
Kelly Narboux <kellynarboux@hotmail.fr>
2019-02-18 17:57:54 +0100

    Get the informations in the form

---
+++ src/main/java/usmb/nc/cgi/Form.java
@@ -0,0 +1,76 @@
+package usmb.nc.cgi;
+
+public class Form {
+    private String last_name;
+    private String first_name;
+    private String birthday;
+    private String adress;
+    private String city;
+    private String postcode;
+    private String phone;
+    private String email;
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/usmb/nc/cgi/FormController.java
+++ src/main/java/usmb/nc/cgi/FormController.java
@@ -1,7 +1,9 @@
 package usmb.nc.cgi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,10 +11,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class FormController {
 
     @GetMapping("/form")
-    public ModelAndView creationForm() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("inscriptionForm");
-        return mv;
+    public String creationForm(Model model) {
+        model.addAttribute("form", new Form());
+        return "inscriptionForm";
     }
 
+    @PostMapping("/form")
+    public String formPost(@ModelAttribute Form form) {
+        return "resultForm";
+    }
 }
