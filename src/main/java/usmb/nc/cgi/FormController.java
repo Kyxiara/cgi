199ffa8981212e5f5a93e88f3f83878cd8273ab6
Kelly Narboux <kellynarboux@hotmail.fr>
2019-03-04 17:37:49 +0100

    WIP : list forms

--- src/main/java/usmb/nc/cgi/FormController.java
+++ src/main/java/usmb/nc/cgi/FormController.java
@@ -1,5 +1,6 @@
 package usmb.nc.cgi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,9 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 @Controller
 public class FormController {
+
+    @Autowired
+    private FormRepository formRepository;
 
     @GetMapping("/form")
     public String creationForm(Model model) {
@@ -22,6 +27,7 @@ public class FormController {
 
     @PostMapping("/form")
     public String formPost(@ModelAttribute Form form) {
+        formRepository.save(form);
         FormFillingPdf formFillingPdf = new FormFillingPdf("src/main/resources/pdf_templates/formulaire_inscription_hockey.pdf");
         try {
             formFillingPdf.fill(form.getHashMap(), "src/main/resources/pdf_templates/inscription_realise.pdf", false);
@@ -44,6 +50,11 @@ public class FormController {
             System.err.println("Error writing file to output stream. Filename was " + fileName);
             throw new RuntimeException("IOError writing file to output stream");
         }
+    }
 
+    @GetMapping("/listforms")
+    public String listForms(Model model) {
+        model.addAttribute("forms", (ArrayList<Form>) formRepository.findAll());
+        return "listForms";
     }
 }
